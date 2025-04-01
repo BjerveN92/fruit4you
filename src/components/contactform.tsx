49b8fcccd69a78c11e9be93d71381c1561c6ca1f
Martin Bjervig <martinbjervig@gmail.com>
2025-04-01 17:47:08 +0200

    added a handleChanger

--- src/components/contactform.tsx
+++ src/components/contactform.tsx
@@ -1,1 +1,20 @@
 import { useState } from "react";
+
+const Contact = () => {
+    const [formData, setFormData] = useState ({
+        name: "",
+        email:"",
+        message:"",
+    });
+
+
+    // handle change , inputändringar
+
+    const handleChange = (e: React.ChangeEvent <HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+
+    // hantera formulär submit 
+
+    // här ska returnen vara
+}
